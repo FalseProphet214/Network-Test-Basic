a2e85f4928db57624ed51e8ca7b0af9079015228
Cj Cox <corbincox@outlook.com>
2023-07-04 15:17:38 -0400

    Fixed issue, now need to account for other failed ping statuses

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@ using System.IO;
 
 namespace PingReporter
 {
+    class PingEvent
+    {
+        public bool IsTimeout { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
     class Program
     {
         static void GenerateReport(List<PingEvent> events, DateTime testStartTime, DateTime testEndTime)
@@ -23,8 +29,8 @@ namespace PingReporter
                 bool isPreviousResponse = false;
                 int timeoutCount = 0;
                 TimeSpan longestTimeoutPeriod = TimeSpan.Zero;
-                DateTime timeoutStart = DateTime.MinValue;
-                DateTime previousEventTime = DateTime.MinValue;
+                DateTime timeoutStart = DateTime.Now;
+                DateTime previousEventTime = DateTime.Now;
 
                 foreach (PingEvent pingEvent in events)
                 {
@@ -37,7 +43,7 @@ namespace PingReporter
                             isPreviousResponse = false;
                             timeoutCount++;
 
-                            if (timeoutStart == DateTime.MinValue)
+                            if (timeoutStart != pingEvent.Timestamp)
                             {
                                 timeoutStart = pingEvent.Timestamp;
                             }
@@ -58,7 +64,7 @@ namespace PingReporter
                                 longestTimeoutPeriod = timeoutPeriod;
                             }
 
-                            timeoutStart = DateTime.MinValue;
+                            // timeoutStart = DateTime.Now;
                         }
                     }
 
@@ -91,6 +97,7 @@ namespace PingReporter
 
             Console.Write("Enter the IP address to ping: ");
             string ipAddress = Console.ReadLine();
+            ipAddress = "192.168.50.154";
 
             string command = "ping " + ipAddress + " -t";
 
